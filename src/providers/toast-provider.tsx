2a6b465772a2ba9068b18501565d4c5196e2799f
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-12-02 23:52:49 +0000

    chore/add providers in root layout

---
+++ src/providers/toast-provider.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Toaster } from "@/components/ui/sonner";
+
+// we need to load the toaster client-side otherwise the positioning resets for some reason
+// - Note: might just be a dev thing
+export function ToastProvider({ children }: { children: React.ReactNode }) {
+	return (
+		<>
+			{children}
+
+			<Toaster position="top-center" duration={1500} closeButton />
+		</>
+	);
+}
