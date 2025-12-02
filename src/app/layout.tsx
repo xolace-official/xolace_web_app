cea671269af7fe4d895119f0f5f3e9ad71dceeb6
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-12-02 17:23:14 +0000

    chore/add different font & shadow behaviour

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito } from "next/font/google";
+import { PT_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const nunito = Nunito({
+  variable: "--font-nunito",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const ptSans = PT_Sans({
+  variable: "--font-pt-sans",
   subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -24,9 +26,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${nunito.variable} ${ptSans.variable} antialiased relative`}
       >
         <ThemeProvider
             attribute="class"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { ModeToggle } from "@/components/theme-switcher";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-center justify-center bg-background">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-background sm:items-start">
       <ModeToggle />
         <Image
           className="dark:invert"
@@ -62,7 +62,7 @@ export default function Home() {
             Documentation
           </a>
 
-<Button className="shadow-2xl">
+<Button className="border-primary-border shadow-primary ">
             Sample Button
 </Button>
         </div>
