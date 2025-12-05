0789453796e2d39ca7541f3cc08661fa2cd5504f
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-12-05 05:53:10 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,3 @@
-import { fileURLToPath } from "node:url";
-import { createJiti } from "jiti";
-
-const jiti = createJiti(fileURLToPath(import.meta.url));
-
-// Import env here to validate during build. Using jiti@^1 we can import .ts files :)
-await jiti.import("./src/env");
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -14,3 +6,12 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+// for later use
+// Agent// import { fileURLToPath } from "node:url";
+// import { createJiti } from "jiti";
+
+// const jiti = createJiti(fileURLToPath(import.meta.url));
+
+// Import env here to validate during build. Using jiti@^1 we can import .ts files :)
+// await jiti.import("./src/env");
