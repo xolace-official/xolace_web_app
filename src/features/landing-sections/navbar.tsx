3b6bebee0dea3c1108fb54ded608a0ee4903525c
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-12-08 23:13:12 +0000

    chore: change name

--- src/features/landing-sections/navbar.tsx
+++ src/features/landing-sections/navbar.tsx
@@ -75,7 +75,7 @@ export const Navbar = () => {
                 <SheetTitle className="flex items-center">
                   <Link href="/" className="flex items-center">
                     <ChevronsDown className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
-                    Shadcn
+                    Xolace
                   </Link>
                 </SheetTitle>
               </SheetHeader>
