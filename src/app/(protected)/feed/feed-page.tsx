9a7fc8a05175b813e08c9fdecca924124e1b51c2
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2025-12-10 22:07:36 +0000

    Update feed-page.tsx

--- src/app/(protected)/feed/feed-page.tsx
+++ src/app/(protected)/feed/feed-page.tsx
@@ -3,7 +3,7 @@ import DummyCard from "@/components/extras/dummy-card";
 
 export const FeedPage = () => {
   return (
-    <PageContainer title="Feed" contentClassName="max-sm:px-2">
+    <PageContainer title="Fireside 🔥" contentClassName="max-sm:px-2">
       <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-3 *:data-[slot=card]:bg-linear-to-t *:data-[slot=card]:shadow-xs">
         <DummyCard />
         <DummyCard />
