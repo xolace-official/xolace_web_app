4bb97cbb49d7cdf5641dd24787a822ac9afd8b9d
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2026-01-23 15:38:23 +0000

    Update create-post-page.tsx

--- src/app/(protected)/create-post/create-post-page.tsx
+++ src/app/(protected)/create-post/create-post-page.tsx
@@ -1,11 +1,17 @@
-"use client";
-
 import { PostComposer } from "@/features/composer";
+import { PageContainer } from "@/components/app/page-container";
+import { ThemeSwitcher } from "@/components/shared/theme-switcher";
 
 export const CreatePostPage = () => {
   return (
-    <div className="mx-auto w-full max-w-2xl px-4 py-6">
-      <PostComposer />
-    </div>
+    <PageContainer
+      title="Create Post🔥"
+      contentClassName="max-sm:px-2"
+      actions={<ThemeSwitcher key={"theme-switcher"} />}
+    >
+      <div className="mx-auto w-full max-w-2xl px-4 py-6">
+        <PostComposer />
+      </div>
+    </PageContainer>
   );
 };
