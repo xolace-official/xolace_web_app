8df6463273de403f549b83a19590718f43496ec2
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2026-01-30 01:54:50 +0000

    Update collections-page.tsx

--- src/app/(protected)/collections/collections-page.tsx
+++ src/app/(protected)/collections/collections-page.tsx
@@ -1,10 +1,15 @@
 import { PageContainer } from "@/components/app/page-container";
 import { PageHeading } from "@/components/shared/layout/page-heading";
+import { ThemeSwitcher } from "@/components/shared/theme-switcher";
 import { CollectionsGrid } from "@/features/collections/components/collections-grid";
 
 export const CollectionsPage = () => {
   return (
-    <PageContainer title="Collections" contentClassName="px-2 md:px-0">
+    <PageContainer
+      title="Collections"
+      contentClassName="px-2 md:px-0"
+      actions={<ThemeSwitcher key={"theme-switcher"} />}
+    >
       <PageHeading
         title="Your Collections"
         description="This is your vast collection of saved items"
